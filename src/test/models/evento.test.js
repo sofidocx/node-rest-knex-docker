5dbf887ee94e4e47b805597bd2dd63ef609373ac
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-09-09 19:21:50 -0300

    fix: fix: arrumando erro do linter de formato

--- src/test/models/evento.test.js
+++ src/test/models/evento.test.js
@@ -6,11 +6,10 @@ import Evento from '../../models/eventos.js';
 describe('Testando o modelo Evento', () => {
   const objetoEvento = {
     nome: 'Comunidade das Letras',
-    descricao: 'Evento voltado para quem gosta e cursa a faculdade de letras', 
+    descricao: 'Evento voltado para quem gosta e cursa a faculdade de letras',
     data: '2025-08-09',
     autor_id: 1,
   };
-
   it('Deve instanciar um novo evento', () => {
     const evento = new Evento(objetoEvento);
 
